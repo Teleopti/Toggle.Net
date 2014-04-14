911b6ef833115870d7e4272add3f41c6c8e53850
Roger Kratz <roger.kratz@gmail.com>
2014-04-14 15:01:41 +0200

    ToggleChecker shouldn't allow duplicate feature flag configurations

--- code/nToggle/Internal/ToggleChecker.cs
+++ code/nToggle/Internal/ToggleChecker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace nToggle.Internal
@@ -6,10 +7,26 @@ namespace nToggle.Internal
 	public class ToggleChecker : IToggleChecker
 	{
 		private readonly IDictionary<string, Feature> _metadata;
+		private const string duplicateEx = "At least one flag is defined more than once: {0}";
 
 		public ToggleChecker(IEnumerable<Feature> metadata)
 		{
+			verifyNoDuplicates(metadata);
 			_metadata = metadata.ToDictionary(x => x.FlagName);
+		}
+
+		private static void verifyNoDuplicates(IEnumerable<Feature> orginalFeatures)
+		{
+			var tempHash = new HashSet<string>();
+			var illegalFlags = new HashSet<string>();
+			foreach (var originalFeature in orginalFeatures.Where(originalFeature => !tempHash.Add(originalFeature.FlagName)))
+			{
+				illegalFlags.Add(originalFeature.FlagName);
+			}
+			if (illegalFlags.Any())
+			{
+				throw new InvalidOperationException(string.Format(duplicateEx, string.Join(", ", illegalFlags)));
+			}
 		}
 
 		public bool IsEnabled(string flagName)
---
+++ code/nToggleTests/EnableDisable/DuplicateTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using nToggle.Configuration.Specifications;
+using nToggle.Internal;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace nToggleTests.EnableDisable
+{
+	public class DuplicateTests
+	{
+		[Test]
+		public void ShouldThrowIfMultipleFlagKeysAreFound()
+		{
+			const string flag = "someFlag";
+
+			Assert.Throws<InvalidOperationException>(() =>
+				new ToggleChecker(new[]
+				{
+					new Feature(flag, new FalseSpecification()),
+					new Feature(flag, new FalseSpecification())
+				})
+			);
+		}
+
+		[Test]
+		public void ShouldContainFlagNamesInExceptionMessage()
+		{
+			const string exFlag1 = "someExFlag";
+			const string exFlag2 = "someEx2Flag";
+			const string correctFlag = "someCorrectFlag";
+
+			var exString = Assert.Throws<InvalidOperationException>(() =>
+				new ToggleChecker(new[]
+				{
+					new Feature(exFlag1, new FalseSpecification()),
+					new Feature(exFlag2, new FalseSpecification()),
+					new Feature(correctFlag, new FalseSpecification()),
+					new Feature(exFlag2, new FalseSpecification()),
+					new Feature(exFlag1, new FalseSpecification()),
+
+				})
+			).ToString();
+
+			exString.Should().Contain(exFlag1);
+			exString.Should().Contain(exFlag2);
+			exString.Should().Not.Contain(correctFlag);
+		} 
+	}
+}
