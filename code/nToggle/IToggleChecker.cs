296349cf6b8049c9bf8ad13cd0ed782d7ce2c8bb
Roger Kratz <roger.kratz@gmail.com>
2014-04-14 16:02:21 +0200

    better comment for IToggleChecker

--- code/nToggle/IToggleChecker.cs
+++ code/nToggle/IToggleChecker.cs
@@ -1,12 +1,18 @@
-﻿namespace nToggle
+﻿using nToggle.Internal;
+
+namespace nToggle
 {
 	/// <summary>
 	/// Main interface at runtime.
-	/// Returns <code>true</code> if feature with specified flag name is enabled,
-	/// otherwise <code>false</code>.
 	/// </summary>
 	public interface IToggleChecker
 	{
+		/// <summary>
+		/// Returns <code>true</code> if feature with specified flag name is enabled,
+		/// otherwise <code>false</code>.
+		/// </summary>
+		/// <param name="flagName"><see cref="Feature.FlagName"/></param>
+		/// <returns></returns>
 		bool IsEnabled(string flagName);
 	}
 }
