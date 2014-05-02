fbccebf2d40d874d865dfd4e927a3868b923bc00
Roger Kratz <roger.kratz@gmail.com>
2014-05-02 15:19:58 +0200

    support for multiple parameters

---
+++ code/Toggle.Net.Tests/Stubs/SpecificationWithMultipleParameters.cs
@@ -0,0 +1,23 @@
+﻿using Toggle.Net.Specifications;
+
+namespace Toggle.Net.Tests.Stubs
+{
+	public class SpecificationWithMultipleParameters : ToggleSpecification
+	{
+		public const string ParameterName1 = "TheParameterName1";
+		public const string ParameterName2 = "TheParameterName2";
+		public const string ParameterName3 = "TheParameterName3";
+
+		public override string Name
+		{
+			get { return "ParametersSpecification"; }
+		}
+
+		public override bool IsEnabled(string currentUser)
+		{
+			return bool.Parse(Parameters[ParameterName1]) &&
+				bool.Parse(Parameters[ParameterName2]) &&
+				bool.Parse(Parameters[ParameterName3]);
+		}
+	}
+}
---
+++ code/Toggle.Net.Tests/TextFile/MultipleParametersTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Toggle.Net.Configuration;
+using Toggle.Net.Providers.TextFile;
+using Toggle.Net.Tests.Stubs;
+
+namespace Toggle.Net.Tests.TextFile
+{
+	public class MultipleParametersTest
+	{
+		[Test]
+		public void ShouldBeEnabled()
+		{
+			var content = new[]
+			{
+				"someflag=ParametersSpecification",
+				"someflag.ParametersSpecification." + SpecificationWithMultipleParameters.ParameterName1 + "=true",
+				"someflag.ParametersSpecification." + SpecificationWithMultipleParameters.ParameterName2 + "=true",
+				"someflag.ParametersSpecification." + SpecificationWithMultipleParameters.ParameterName3 + "=true"
+			};
+			var fileProvider = new FileProvider(new FileReaderStub(content));
+			fileProvider.AddSpecification(new SpecificationWithMultipleParameters());
+			var toggleChecker = new ToggleConfiguration(fileProvider).Create();
+			toggleChecker.IsEnabled("someflag")
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldBeDisabled()
+		{
+			var content = new[]
+			{
+				"someflag=ParametersSpecification",
+				"someflag.ParametersSpecification." + SpecificationWithMultipleParameters.ParameterName1 + "=true",
+				"someflag.ParametersSpecification." + SpecificationWithMultipleParameters.ParameterName2 + "=false",
+				"someflag.ParametersSpecification." + SpecificationWithMultipleParameters.ParameterName3 + "=true"
+			};
+			var fileProvider = new FileProvider(new FileReaderStub(content));
+			fileProvider.AddSpecification(new SpecificationWithMultipleParameters());
+			var toggleChecker = new ToggleConfiguration(fileProvider).Create();
+			toggleChecker.IsEnabled("someflag")
+				.Should().Be.False();
+		}  
+	}
+}
