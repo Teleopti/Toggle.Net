fc8a431394dab148bd9db284b62d0e575f3a21a8
Roger Kratz <roger.kratz@gmail.com>
2014-06-30 15:22:41 +0200

    adding RegExSpecification

---
+++ code/Toggle.Net.Tests/TextFile/RegExTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Toggle.Net.Configuration;
+using Toggle.Net.Providers.TextFile;
+using Toggle.Net.Specifications;
+using Toggle.Net.Tests.Stubs;
+
+namespace Toggle.Net.Tests.TextFile
+{
+	public class RegExTest
+	{
+		[Test]
+		public void ShouldHandleMatch()
+		{
+			const string wordToMatch = "the toggle";
+
+			var content = new[]
+			{
+				"sometoggle=myRegex",
+				"sometoggle.myRegex." + RegExSpecification.RegExParameter + " = /^" + wordToMatch + "$/"
+			};
+			var mappings = new DefaultSpecificationMappings();
+			mappings.AddMapping("myRegex", new RegExSpecification(wordToMatch));
+
+			var toggleChecker = new ToggleConfiguration(new FileParser(new FileReaderStub(content), mappings))
+				.SetUserProvider(new UserProviderStub("sometoggle"))
+				.Create();
+
+			toggleChecker.IsEnabled("sometoggle")
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldHandleMiss()
+		{
+			const string wordToMatch = "the toggle";
+
+			var content = new[]
+			{
+				"sometoggle=myRegex",
+				"sometoggle.myRegex." + RegExSpecification.RegExParameter + " = /^" + wordToMatch + "$/"
+			};
+			var mappings = new DefaultSpecificationMappings();
+			mappings.AddMapping("myRegex", new RegExSpecification(wordToMatch+"somethingelse"));
+
+			var toggleChecker = new ToggleConfiguration(new FileParser(new FileReaderStub(content), mappings))
+				.SetUserProvider(new UserProviderStub("sometoggle"))
+				.Create();
+
+			toggleChecker.IsEnabled("sometoggle")
+				.Should().Be.False();
+		}
+
+		[Test]
+		public void ShouldThrowIfRegExIsMissing()
+		{
+			const string wordToMatch = "the toggle";
+
+			var content = new[]
+			{
+				"sometoggle=myRegex"
+			};
+			var mappings = new DefaultSpecificationMappings();
+			mappings.AddMapping("myRegex", new RegExSpecification(wordToMatch));
+
+			Assert.Throws<IncorrectTextFileException>(() => 
+				new ToggleConfiguration(new FileParser(new FileReaderStub(content), mappings))
+				.SetUserProvider(new UserProviderStub("sometoggle")).Create()).ToString()
+			.Should().Contain(string.Format(RegExSpecification.MustDeclareRegexPattern, "sometoggle"));
+		}
+	}
+}
---
+++ code/Toggle.Net/Specifications/RegExSpecification.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Toggle.Net.Specifications
+{
+	public class RegExSpecification : IToggleSpecification
+	{
+		public const string MustDeclareRegexPattern = "Missing parameter '" + RegExParameter + "' for Feature '{0}'.";
+		public const string RegExParameter = "pattern";
+
+		private readonly Regex _regex;
+
+		public RegExSpecification(string regex)
+		{
+			_regex = new Regex(regex);
+		}
+
+		public bool IsEnabled(string currentUser, IDictionary<string, string> parameters)
+		{
+			return _regex.IsMatch(parameters[RegExParameter]);
+		}
+
+		public void Validate(string toggleName, IDictionary<string, string> parameters)
+		{
+			if (!parameters.ContainsKey(RegExParameter))
+				throw new InvalidSpecificationParameterException(string.Format(MustDeclareRegexPattern, toggleName));
+		}
+	}
+}
