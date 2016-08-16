c9239bb947c9b70b6fcc12d14fd3396547174827
Roger Kratz <roger.kratz@gmail.com>
2016-08-16 14:04:05 +0200

    No need to keep scripts to run tests

--- scripts/RunTests/runtests.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET SRCDIR=%~dp0
-set SRCDIR=%SRCDIR:~0,-18%
-set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-
-%msbuild% runtests.msbuild /p:RootFolder=%srcdir%
-
-echo.
-pause
