d6128ee839559f49cb43947d9fc3608b5c082a98
Roger Kratz <roger.kratz@gmail.com>
2019-03-27 13:25:15 +0100

    upgrade to .net45 and multi target to netstandard2.0

--- scripts/buildRelease/buildRelease.bat
+++ scripts/buildRelease/buildRelease.bat
@@ -2,23 +2,10 @@
 SET SRCDIR=%~dp0
 set SRCDIR=%SRCDIR:~0,-22%
 
-
-set nugetfolder="%SRCDIR%\code\.nuget"
-set packageFolder="%SRCDIR%\code\packages"
-set msbuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-set configuration=Release
-set msbuildtasksVersion=1.4.0.65
-
-
-echo Installing msbuildtasks to %PackageFolder%. Please wait...
-%nugetFolder%\NuGet install MsBuildTasks -o %PackageFolder% -Version %msbuildtasksVersion%
-
 echo.
 set /p Version=Please enter version number, eg 1.2.0: 
 
-%msbuild% nugetPackage.msbuild /v:q /t:BuildRelease
-
-git checkout -- %SRCDIR%\code\Toggle.Net\Properties\AssemblyInfo.cs
+dotnet msbuild nugetPackage.msbuild /t:BuildRelease
 
 echo -------------------------------
 echo.
